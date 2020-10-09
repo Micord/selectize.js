3300a107f5706ae87b0c7986cf0c1960508dae76
Tihomirov Eduard <tikhomirov@micord.ru>
2020-10-09 10:18:48 +0300

    SUPPORT-5237: Add infinite_scroll plugin and up version

--- dist/js/selectize.js
+++ dist/js/selectize.js
@@ -1,5 +1,5 @@
 /**
- * selectize.js (v0.12.4-cg.4)
+ * selectize.js (v0.12.4-cg.5)
  * Copyright (c) 2013–2015 Brian Reavis & contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
@@ -2982,6 +2982,68 @@
 			};
 		})();
 	
+	});
+	
+	Selectize.define('infinite_scroll', function(options) {
+	  var self = this
+	      , page = 0;
+	
+	  self.infinitescroll = {
+	    onScroll: function() {
+	      var scrollBottom = self.$dropdown_content[0].scrollHeight - (self.$dropdown_content.scrollTop() + self.$dropdown_content.height())
+	      if(scrollBottom < 100){
+	        var query = JSON.stringify({
+	          search: self.lastValue,
+	          page: page
+	        })
+	
+	        self.$dropdown_content.off('scroll')
+	        var queryObject = JSON.parse(query)
+	        if (queryObject.search != "") {
+	          self.onSearchChange(query)
+	        }
+	      }
+	    }
+	  };
+	
+	  self.onFocus = (function() {
+	    var original = self.onFocus;
+	
+	    return function() {
+	      var query = JSON.stringify({
+	        search: self.lastValue,
+	        page: page
+	      })
+	
+	      original.apply(self, arguments);
+	      // self.onSearchChange(query)
+	    };
+	  })();
+	
+	  self.onKeyUp = function(e) {
+	    var self = this;
+	
+	    if (self.isLocked) return e && e.preventDefault();
+	    var value = self.$control_input.val() || '';
+	
+	    if (self.lastValue !== value) {
+	      var query = JSON.stringify({
+	        search: value,
+	        page: page = 0
+	      });
+	
+	      self.lastValue = value;
+	      self.onSearchChange(query);
+	      self.refreshOptions();
+	      self.clearOptions();
+	      self.trigger('type', value);
+	    }
+	  };
+	
+	  self.on('load',function(){
+	    page++
+	    self.$dropdown_content.on('scroll', self.infinitescroll.onScroll);
+	  });
 	});
 	
 	Selectize.define('input_max_length', function(options) {
---
+++ src/plugins/infinite_scroll/plugin.js
@@ -0,0 +1,61 @@
+Selectize.define('infinite_scroll', function(options) {
+  var self = this
+      , page = 0;
+
+  self.infinitescroll = {
+    onScroll: function() {
+      var scrollBottom = self.$dropdown_content[0].scrollHeight - (self.$dropdown_content.scrollTop() + self.$dropdown_content.height())
+      if(scrollBottom < 100){
+        var query = JSON.stringify({
+          search: self.lastValue,
+          page: page
+        })
+
+        self.$dropdown_content.off('scroll')
+        var queryObject = JSON.parse(query)
+        if (queryObject.search != "") {
+          self.onSearchChange(query)
+        }
+      }
+    }
+  };
+
+  self.onFocus = (function() {
+    var original = self.onFocus;
+
+    return function() {
+      var query = JSON.stringify({
+        search: self.lastValue,
+        page: page
+      })
+
+      original.apply(self, arguments);
+      // self.onSearchChange(query)
+    };
+  })();
+
+  self.onKeyUp = function(e) {
+    var self = this;
+
+    if (self.isLocked) return e && e.preventDefault();
+    var value = self.$control_input.val() || '';
+
+    if (self.lastValue !== value) {
+      var query = JSON.stringify({
+        search: value,
+        page: page = 0
+      });
+
+      self.lastValue = value;
+      self.onSearchChange(query);
+      self.refreshOptions();
+      self.clearOptions();
+      self.trigger('type', value);
+    }
+  };
+
+  self.on('load',function(){
+    page++
+    self.$dropdown_content.on('scroll', self.infinitescroll.onScroll);
+  });
+});
